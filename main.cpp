e0a8339602dbd66083a413038ebe8b9615d7b8d2
DarrylMcCottrell <43862357+DarrylMcCottrell@users.noreply.github.com>
2019-02-26 22:45:20 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -17,40 +17,51 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 	// reading a inputfile
-	cout << "Argc:  " << argc << endl;
+	cout << "Argc:  " << argc << endl; // checking argc for the right value
 	for (int index = 0; index < argc; index++)
 		cout << "Index: " << index << " value: " << argv[index] << endl;
 
-	if (argc < 2) {
+	if (argc < 2) { 
 		cerr << "Need to provide a file name" << endl;
 		return 0;
 	}
-	ifstream infile(argv[1]);
+	ifstream infile(argv[1]); // getting the filename for argv[1], opening the file properly
 	if(infile.is_open()){
 		string line;
+		int sumOfInnerList = 0;
+	
+		list<int> *innerList = new list<int> ();
+		list<list<int> *> outer_list;
 		while(getline(infile,line)) {
-			cout << line << endl;
-		}
-		infile.close();
+					stringstream ss(line);
+			int totalSum = 0;
+		
+	
+		do{
+			int numbers;
+			ss >> numbers;
+			totalSum += numbers; //totalsum = totalsum + numbers
+			cout << "Number is: " << numbers << endl;
+			 (*innerList).push_back(numbers);
+			 sumOfInnerList++; 
+			
+		} while(!ss.eof());
+			outer_list.push_back(innerList);
+	}	
+
+	 cout << "List Counter: " << sumOfInnerList << endl; 
+			 cout << "Size of the Outer List: " << sumOfInnerList<< endl;
+	for(int i = 0; i < outer_list.size(); i++ ){
+		cout << "List" << (i + 1) << "has" << (i + 1) << "elements and sums to" << (i + 3) << endl;
+	}
+
+	infile.close(); // Closing the inputfile
+
 	}
 	else {
 		cerr << " Unable to provide a file" << endl;
 	}
 	
-	string line;
-	while(getline(cin,line)) {
-		stringstream ss(line);
-		do{
-			int numbers;
-			ss >> numbers;
-			cout << "is: " << numbers << endl;
-			// would the inner list begin here?
-
-
-			
-		} while(!ss.eof());
-			{
-				//would the outterlist begin here or outside the loop?
-		}
+	
 	return 0;
 }
